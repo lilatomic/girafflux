5636756cb50ed93e7220f8bbb7f987573ba0d6e2
Daniel Goldman <danielgoldman4@gmail.com>
2022-12-31 20:49:52 -0500

    add framework for testing converting Girafflux to Flux

---
+++ src/test/scala/il/Complete.scala
@@ -0,0 +1,33 @@
+package il
+
+import flux.Printer
+import giraffe.{GLexer, GParser}
+
+import scala.io.Source
+
+
+class Complete extends munit.FunSuite {
+  def check(filename: String)(implicit loc: munit.Location): Unit = {
+    test(s"transform $filename") {
+      val file = Source.fromInputStream(getClass.getResourceAsStream("/il/simplest")).mkString
+      val sections = file.split("---").filter(_.nonEmpty).map(section => section(0) -> section.substring(1)).toMap
+
+      val fStr = for {
+        gTokens <- GLexer.lex(sections('g'))
+        gAst <- GParser.parse(gTokens)
+        fAst <- Transformer.transformProgram(gAst)
+        fStr <- Right(Printer.print(fAst))
+      } yield fStr
+
+      val expected = sections('f')
+
+      // ---
+      val obtained = fStr.map(_.mkString("\n")).getOrElse("")
+      pprint.pprintln(obtained)
+      assertEquals(obtained, expected)
+    }
+  }
+
+  check("/li/simplest")
+
+}
